a815feaff775e6eddfc66efca63fb16e22a3c540
huisheng.jin <huisheng.jinge@gmail.com>
2021-08-13 09:32:07 +0800

    refactor: wordCounter

--- src/main/java/kata/WardCounter.java
+++ src/main/java/kata/WardCounter.java
@@ -1,43 +1,39 @@
 package kata;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toMap;
 
 public class WardCounter {
-    private String text;
+    private final String text;
 
     public WardCounter(String text) {
         this.text = text;
     }
 
     public String wardCounting() {
-        HashMap<String, Integer> wardStorage = new HashMap<>();
-        addToWardMap(wardStorage);
-
-        List<Map.Entry<String, Integer>> sortedWardList = new ArrayList<>(wardStorage.entrySet());
-        Collections.sort(sortedWardList, (o1, o2) -> o2.getValue()-o1.getValue());
-
-        String output = "";
-        for (Map.Entry pair : sortedWardList) {
-            output = formatWardList(output, pair);
-        }
-        return output;
+        String[] wordsArray = text.split("\\s+");
+        Map<String, Integer> groupMap = group(wordsArray);
+        Stream<Map.Entry<String, Integer>> sort = sort(groupMap);
+        return format(sort);
     }
 
-    private String formatWardList(String output, Map.Entry pair) {
-        if (!output.isEmpty()) {
-            output += "\r\n";
-        }
-        output += pair.getKey();
-        output += " ";
-        output += pair.getValue();
-        return output;
+    private Map<String, Integer> group(String[] wordsArray) {
+        return Arrays.stream(wordsArray).collect(groupingBy(item -> item)).entrySet().stream()
+                .collect(toMap(Map.Entry::getKey, item -> item.getValue().size()));
     }
 
-    private void addToWardMap(HashMap<String, Integer> wardStorage) {
-        for (String ward : text.split("\\s+")) {
-            if (wardStorage.putIfAbsent(ward, 1) != null) {
-                wardStorage.put(ward, wardStorage.get(ward) + 1);
-            }
-        }
+    private Stream<Map.Entry<String, Integer>> sort(Map<String, Integer> group) {
+        return group.entrySet().stream()
+                .sorted((o1, o2) -> o2.getValue() - o1.getValue());
+    }
+
+    private String format(Stream<Map.Entry<String, Integer>> sortedWardList) {
+        return sortedWardList
+                .map(pair -> pair.getKey() + " " + pair.getValue()).collect(Collectors.joining("\r\n"));
     }
 }
